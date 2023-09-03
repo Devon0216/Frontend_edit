1a18b5c07b25ca609d6df437afee6b32f793ae0f
devon <devonyeung0216@gmail.com>
2023-09-03 18:36:59 +0200

    disable buttons

--- src/features/help/Help.js
+++ src/features/help/Help.js
@@ -3,7 +3,19 @@ const Help = () => {
     let content = (
         <section>
             <h1 className="sectionHeading">Help documentation:</h1>
-            <p></p>
+            <p> 
+                You are acting as a facilitator or a coach of a Design Thinking workshop. 
+                This tool is to help you to better manage the workshop, with usage of a Miro shared whiteboard.
+                You are interacting with a Miro shared whiteboard, so you have to authorise through Miro first to use this application.
+            </p>
+
+            <h1 className="sectionHeading">Sticky Notes feature:</h1>
+            <p> 
+                Get Participants Notes: You can view the sticky notes contents(added by workshop participants) on the Miro board.
+                <br></br>
+                Save sticky ntoes to workshop: You can save sticky notes to the current workshop. (can be viewed in Past Workshop page)
+                Summarise the notes: You can summarise the sticky notes contents, and save the summary to the current workshop. (can be viewed in Past Workshop page)
+            </p>
 
 
 
