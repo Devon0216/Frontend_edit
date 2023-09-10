681a39f39e889c3a6c23a056d7ace4244ed38217
devon <devonyeung0216@gmail.com>
2023-09-10 17:17:34 +0200

    fixed axios

--- src/features/stickynotes/StickyNotes.js
+++ src/features/stickynotes/StickyNotes.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const getStickyNotes = async (access_token, boardID) => {
     const options = {
         'method': 'GET',
--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const createUser = async (username, miroId) => {
     const options = {
         'method': 'POST',
