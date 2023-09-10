756a559db629019b5eccccbe80958e326ce4d812
devon <devonyeung0216@gmail.com>
2023-09-10 16:07:47 +0200

    take out store

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://whiteboarddj-server.onrender.com' }),
-    tagTypes: ['Note', 'User'],
-    endpoints: builder => ({})
-})
+// export const apiSlice = createApi({
+//     baseQuery: fetchBaseQuery({ baseUrl: 'https://whiteboarddj-server.onrender.com' }),
+//     tagTypes: ['Note', 'User'],
+//     endpoints: builder => ({})
+// })
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { store } from './app/store'
+// import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
+    <Provider >
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<App />} />
