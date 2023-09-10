681176e7f9cee2354c7e8326a5e2df7b71ef623d
devon <devonyeung0216@gmail.com>
2023-09-10 16:24:37 +0200

    undo on store

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// import { store } from './app/store'
+import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <Provider >
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<App />} />
